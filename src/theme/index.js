6992af2b169a32b2a18e2d9a525aba4277270210
G-Gamboa <gamboaguillermo12@gmail.com>
2026-04-08 17:24:39 -0600

    Paletas

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,5 @@
-import { paletteGreen, palettePink, paletteNeutral } from "./palettes";
-
-export function getPaletteByUser(userId) {
-  const id = String(userId);
-
-  if (id === "1282471582") return paletteGreen;
-  if (id === "5592032215") return palettePink;
-
-  return paletteNeutral;
-}
+export{
+  getPaletteByKey,
+  getPaletteByUser,
+  getPaletteOptions,
+} from './palettes'
